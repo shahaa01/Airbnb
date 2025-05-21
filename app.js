b77fe40c15ab866bf7bdf9757e254d75b37ba1cd
Aaditya Shah <aadishah1020@gmail.com>
2025-05-21 15:32:05 +0545

    Added edit and add option

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const mongoose = require('mongoose');
 const PORT = 8080;
 const app = express();
 const path = require('path');
+const methodOverride = require('method-override');
 const Listing = require('./models/listing');
 
 //lets set ejs and required middlewares here
 app.set("view engine", 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
+app.use(express.urlencoded({extended: true})); //to parse form data
+app.use(express.json()); //for JSON data
 
 //connecting to the database here
 main().then(() => console.log('Database Connected Successfully🚀')).catch(err => console.log(err));
@@ -47,9 +51,42 @@ app.get('/individualListing/:id', async(req, res) => {
 
 });
 
-//route to edit listing 
+//route to get edit form 
 app.get('/editList/:id', async (req, res) => {
-  res.send('editForm');
+  const {id} = req.params;
+  const requiredListing = await Listing.findById(id);
+  res.render('editForm', {listing: requiredListing});
+});
+
+//route to update from the edit form
+app.put('/editList/:id', async (req, res) => {
+  try {
+      const {id} = req.params;
+      const requiredListing = await Listing.findByIdAndUpdate(id, req.body.listing);
+      res.redirect(`/individualListing/${id}`);
+  } catch(err) {
+    console.log(`Error in update route - ${err.message}`);
+  }
+
+});
+
+//route to show add new list form
+app.get('/addList', async (req, res) => {
+  res.render('newListingForm');
+});
+
+//route to add in the db
+app.post('/addList', async(req, res) => {
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect('/airbnbClone');
+});
+
+//route to delete
+app.delete('/deleteList', async(req, res) => {
+  const {id} = req.params;
+  await Listing.findByIdAndDelete(id);
+  res.redirect('/airbnbClone()')
 })
 
 
