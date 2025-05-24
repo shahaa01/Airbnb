03c53ba70340e486d5bac3e6575ec59aeb59498a
Aaditya Shah <aadishah1020@gmail.com>
2025-05-25 00:16:55 +0545

    Improved project structure by moving Listing Schema Validations in Validations directory

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const engine = require('ejs-mate');
 const ExpressErr = require('./errors/expressErr');
 const asyncWrap = require('./utils/asyncWrap');
 const joi = require('joi');
-const ListingServerSchema = require('./schemaValidation');
+const ListingServerSchema = require('./Validations/schemaValidation');
 
 //lets set ejs and required middlewares here
 app.set("view engine", 'ejs');
