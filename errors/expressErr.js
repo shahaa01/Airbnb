c3cdbff368b9e1784f59b11e9daabc0cad027b83
Aaditya Shah <aadishah1020@gmail.com>
2025-05-25 00:14:39 +0545

    Fixed spelling of extremely

--- errors/expressErr.js
+++ errors/expressErr.js
@@ -1,5 +1,5 @@
 class ExpressErr extends Error {
-    constructor(status = 500, message = "Something went wrong, we are extemely sorry!", redirectLink = "/airbnbClone") {
+    constructor(status = 500, message = "Something went wrong, we are extremely sorry!", redirectLink = "/airbnbClone") {
         super();
         this.message = message;
         this.status = status;
