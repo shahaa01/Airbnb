0469de17365f0cb30f99eeb36f4044e149e5529d
Aaditya Shah <aadishah1020@gmail.com>
2025-05-25 11:31:06 +0545

    Removed prac.js which was an unwanted file

--- prac.js
+++
@@ -1,5 +0,0 @@
-const p = Promise.reject(Promise.resolve("aadi"));
-
-Promise.resolve(p).then(data => {
-  console.log("✅ Still hi:", data); // "hi"
-}).catch(err => console.log(`error is ${err.message}`));
