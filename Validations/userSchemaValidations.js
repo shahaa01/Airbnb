1af15542b4adfb744eeb5029c2b8401deebee7d5
Aaditya Shah <aadishah1020@gmail.com>
2025-06-05 17:57:26 +0545

    Fixed few layout issues and added signup feature

--- Validations/userSchemaValidations.js
+++ Validations/userSchemaValidations.js
@@ -12,14 +12,35 @@ const userServerSchema = joi.object({
             'string.empty' : 'Name field cannot be empty. Please enter a valid name.',
             'any.required' : 'Name field cannot be empty. Please enter a valid name.'
         }),
-        email: joi.string()
-        .custom(notValidEmail)
+        username: joi.string()
+        .custom(onlyNumbers)
         .required()
         .messages({
-            'string.notValidEmail' : 'Please enter a valid email address.',
+            'string.onlyNumbers' : 'Username cannot just be a number. Please enter a valid alphanumeric or alphabetical username.',
+            'string.base' : 'Please enter a valid alphanumeric or alphabetical username.',
+            'string.empty' : 'Username field cannot be empty. Please enter a valid username.',
+            'any.required' : 'Username field cannot be empty. Please enter a valid username.'
+        }),
+        email: joi.string()
+        .email({tlds: {allow: false}}) //only during development for testing purposes
+        .required()
+        .messages({
+            'string.email' : 'Please enter a valid email address.',
             'string.base' : 'Please enter a valid email address.',
             'string.empty' : 'Email field cannot be empty. Please enter a valid email.',
             'any.required' : 'Email field cannot be empty. Please enter a valid email.'
+        }),
+        password: joi.string()
+        .required()
+        .messages({
+            'string.empty' : 'Password field cannot be empty. Please enter a valid password.',
+            'any.required' : 'Password field cannot be empty. Please enter a valid password.'
+        }),
+        confirmPassword: joi.string()
+        .required()
+        .messages({
+            'string.empty' : 'Password field cannot be empty. Please enter a valid password.',
+            'any.required' : 'Password field cannot be empty. Please enter a valid password.'
         })
         
     }).required()
@@ -33,12 +54,13 @@ function containsDigit(value, helper) {
     return value;
 }
 
-function notValidEmail(value, helper) {
-    if(/[\w.@-]+/.test(value)) {
-        return value;
+function onlyNumbers(value, helper) {
+    if(/^\d+$/.test(value)) {
+        return helper.error('string.onlyNumbers');
     }
 
-    return helper.error('string.notValidEmail');
+    return value;
 }
+
 
 module.exports = userServerSchema;
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,22 @@
 const ExpressErr = require('../errors/expressErr');
 const User = require('../models/user');
 const passport = require('passport');
+const asyncWrap = require('../utils/asyncWrap');
+const flash = require('connect-flash');
 
 
 module.exports.signUp = async(req, res) => {
     res.render('pages/signUpForm');
 }
 
-module.exports.saveUser = async(req, res) => {
+module.exports.saveUser = asyncWrap(async(req, res, next) => {
+    if(req.body.user.password !== req.body.user.confirmPassword) {
+        return next(new ExpressErr(400, 'Passwords did not match, please type correctly!', '/auth/signUp'));
+    }
     const user = {...req.body.user};
     delete user.password;
     delete user.confirmPassword;
-    const registeredUser = User.register(new User(user), req.body.user.password);
-    res.send(registeredUser);
-}
+    const registeredUser = await User.register(new User(user), req.body.user.password);
+    req.flash('success', `Welcome to airbnb, ${registeredUser.name}!`);
+    res.redirect('/listing');
+});
